fca5c9ef9e9e6dd85586294d02c950f16b75872e
Lunjalook <azra.cosickic@gmail.com>
2022-04-18 09:12:56 +0200

    Adding exercise 04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,12 +1,14 @@
-// Only change code below this line
 const milili = [10, 25, 4];
+
 function myArr(mili) {
-    "use strict";
-    mili.push(mili.shift());
-    mili.push(mili.shift());
+    'use strict';
+
+    milili[0] = 4;
+    milili[1] = 10;
+    milili[2] = 25;
     return mili;
 }
-// Using mili = [4, 10, 25] would be invalid
+
 console.log(myArr(milili));
-// Only change code above this line
+
 module.exports = myArr;
